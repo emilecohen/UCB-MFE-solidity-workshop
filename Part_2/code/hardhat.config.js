16d5e61c191b421bae881f9b2ac4b629c0ac8243
Emile Cohen <emile@onlyalt.com>
2022-04-21 08:18:51 -0500

    Part_2 NFT

--- Part_2/code/hardhat.config.js
+++ Part_2/code/hardhat.config.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 require('@nomiclabs/hardhat-waffle');
 require('@nomiclabs/hardhat-etherscan');
+require("hardhat-gas-reporter");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -19,7 +20,7 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: '0.8.9',
+  solidity: '0.8.10',
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || '',
